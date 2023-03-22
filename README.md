abf359287b372c48d1a24ad430e4a6c0f34d4c3b
Darina Y <darina.yarkaeva@gmail.com>
2023-03-22 14:33:22 +0500

    Started new project

---
+++ README.md
@@ -0,0 +1,3 @@
+# fetching_data_with_cat_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fetching_data_with_cat_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fetching_data_with_cat_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
